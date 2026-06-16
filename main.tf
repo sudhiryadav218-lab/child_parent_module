35f929faad5e1fafefacc0b2ff3f21e91a1426f1
Sudhir Yadav <sudhiryadav218@gmail.com>
2026-06-17 00:55:35 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,35 +1,8 @@
-module "resource_group" {
-  source = "../module/resource_group"
-
-  rgs = {
-    rg1 = {
-      name     = "sky0710"
-      location = "eastus"
-    }
-
-    rg2 = {
-      name     = "sky0711"
-      location = "eastus"
-    }
-  }
+resource "azurerm_resource_group" "sky0710" {
+ 
+    for_each = var.rgs
+    name     = each.value.name
+    location = each.value.location
+  
 }
 
-module "storage_account" {
-  source = "../module/storage_account"
-
-  storage_accounts = {
-    st1 = {
-      name                = "skystorage0710"
-      resource_group_name = module.resource_group.rgs["rg1"].name
-      location            = module.resource_group.rgs["rg1"].location
-      account_tier        = "Standard"
-    }
-
-    st2 = {
-      name                = "skystorage0711"
-      resource_group_name = module.resource_group.rgs["rg2"].name
-      location            = module.resource_group.rgs["rg2"].location
-      account_tier        = "Standard"
-    }
-  }
-}
